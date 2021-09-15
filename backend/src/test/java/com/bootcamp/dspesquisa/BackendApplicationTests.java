9ff416b8eefa9aa47f86916f914cf7605bfcdcb5
Filipe de Deus <filipedeus2015@gmail.com>
2021-09-15 10:43:47 -0300

    Project created

---
+++ backend/src/test/java/com/bootcamp/dspesquisa/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
